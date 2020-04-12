48a2a87193f3e994c570b1a0b9bdd896351b996c
lzmoziming <moziming@189.cn>
2020-04-12 10:53:21 +0800

    返回INDEX页面

--- 01 网络编程/http-1.py
+++ 01 网络编程/http-1.py
@@ -3,12 +3,19 @@ import socket
 
 def service_client(new_socket):
     request = new_socket.recv(1024)
+    print(">>>>>>>>>>>>>"*2)
     print(request)
 
     response = "HTTP/1.1 200 OK\r\n"
     response += "\r\n"
-    response += "<h1>hahahaha</h1>"
+    # response += "<h1>hahahaha</h1>"
+    f = open('./html/index.html', 'rb')
+    html_content = f.read()
+    f.close()
+
     new_socket.send(response.encode("utf-8"))
+    new_socket.send(html_content)
+
     new_socket.close()
 
 
@@ -20,7 +27,6 @@ def main():
     tcp_server_socket.listen()
 
     while True:
-
         new_socket, client_addr = tcp_server_socket.accept()
 
         service_client(new_socket)
